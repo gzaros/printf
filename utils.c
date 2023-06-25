9e247e22963945565c114264441ed3d863558bd9
gzaros <templatewebvo@gmail.com>
2023-06-25 12:36:20 -0700

    utils.c

---
+++ utils.c
@@ -0,0 +1,87 @@
+#include "main.h"
+
+/**
+ * is_printable - Checks if a character is printable.
+ * @c: Character evaluation.
+ *
+ * Return: 1 if c is printable, 0 otherwise.
+ */
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * append_hexa_code - Append the ASCII value as a hex to the buffer.
+ * @buffer: Array of chars
+ * @ascii_code: ASSCI CODE
+ * @i: Append starting point
+ * Return: Always 3
+ */
+int append_hexa_code(char ascii_code, char buffer[], int i)
+{
+	char map_to[] = "0123456789ABCDEF";
+	/* Hexadecimal format codes are always two digits long. */
+
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
+	return (3);
+}
+
+/**
+ * is_digit - Verifies if a char is a digit
+ * @c: Char to be evaluated
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+
+/**
+ * convert_size_number - Casts a number to the specified size
+ * @num: Number to cast
+ * @size: Number indicating type to cast
+ *
+ * Return: value of num casted
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
+}
+
+/**
+ * convert_size_unsgnd - Type-casts a number to the specified size.
+ * @number: Number to cast
+ * @size: Type-casting indicator for the number.
+ * Return: value of num casted
+ */
+long int convert_size_unsgnd(unsigned long int number, int size)
+{
+	if (size == S_LONG)
+		return (number);
+	else if (size == S_SHORT)
+		return ((unsigned short)number);
+
+	return ((unsigned int)number);
+}
+

d4d8871b870bebbdd508667f94640833bba9a2b8
gzaros <templatewebvo@gmail.com>
2023-06-26 22:08:36 +0100

    Fixed Error

--- write_funcs.c
+++ write_funcs.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
+ * _putchar - Writes the 'c' to the standard output stdout.
  * @c: The character to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
- * Description: _putchar uses a local buffer of 1024 to call write
+ * Description: _putchar utilizes a local buffer of size 1024.
  * as little as possible
  */
 int _putchar(char c)
